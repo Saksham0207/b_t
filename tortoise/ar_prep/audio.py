fa7ac455fa1048846e8e347b0913c799db56d074
Saksham Sharma <saksham.sharma.met19@iitbhu.ac.in>
2023-10-27 15:30:18 +0530

    final code clean

--- tortoise/ar_prep/audio.py
+++ tortoise/ar_prep/audio.py
@@ -1,9 +1,7 @@
 import os
 from glob import glob
 
-import librosa
 import torch
-import torchaudio
 import numpy as np
 from scipy.io.wavfile import read
 import boto3
@@ -19,54 +17,8 @@ bucket_name = "tortoise-test"
 session = boto3.Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 s3 = session.client('s3')
 
-def load_wav_to_torch(full_path):
-    sampling_rate, data = read(full_path)
-    if data.dtype == np.int32:
-        norm_fix = 2 ** 31
-    elif data.dtype == np.int16:
-        norm_fix = 2 ** 15
-    elif data.dtype == np.float16 or data.dtype == np.float32:
-        norm_fix = 1.
-    else:
-        raise NotImplemented(f"Provided data dtype not supported: {data.dtype}")
-    return (torch.FloatTensor(data.astype(np.float32)) / norm_fix, sampling_rate)
-
-
-def load_audio(audiopath, sampling_rate):
-    if audiopath[-4:] == '.wav':
-        audio, lsr = load_wav_to_torch(audiopath)
-    elif audiopath[-4:] == '.mp3':
-        audio, lsr = librosa.load(audiopath, sr=sampling_rate)
-        audio = torch.FloatTensor(audio)
-    else:
-        assert False, f"Unsupported audio format provided: {audiopath[-4:]}"
-
-    # Remove any channel data.
-    if len(audio.shape) > 1:
-        if audio.shape[0] < 5:
-            audio = audio[0]
-        else:
-            assert audio.shape[1] < 5
-            audio = audio[:, 0]
-
-    if lsr != sampling_rate:
-        audio = torchaudio.functional.resample(audio, lsr, sampling_rate)
-
-    # Check some assumptions about audio range. This should be automatically fixed in load_wav_to_torch, but might not be in some edge cases, where we should squawk.
-    # '2' is arbitrarily chosen since it seems like audio will often "overdrive" the [-1,1] bounds.
-    if torch.any(audio > 2) or not torch.any(audio < 0):
-        print(f"Error with {audiopath}. Max={audio.max()} min={audio.min()}")
-    audio.clip_(-1, 1)
-
-    return audio.unsqueeze(0)
-
-
 TACOTRON_MEL_MAX = 2.3143386840820312
 TACOTRON_MEL_MIN = -11.512925148010254
-
-
-def denormalize_tacotron_mel(norm_mel):
-    return ((norm_mel+1)/2)*(TACOTRON_MEL_MAX-TACOTRON_MEL_MIN)+TACOTRON_MEL_MIN
 
 
 def normalize_tacotron_mel(mel):
@@ -91,34 +43,12 @@ def dynamic_range_decompression(x, C=1):
     return torch.exp(x) / C
 
 
-def get_voices(extra_voice_dirs=[]):
-    dirs = [BUILTIN_VOICES_DIR] + extra_voice_dirs
-    voices = {}
-    for d in dirs:
-        subs = os.listdir(d)
-        for sub in subs:
-            subj = os.path.join(d, sub)
-            if os.path.isdir(subj):
-                voices[sub] = list(glob(f'{subj}/*.wav')) + list(glob(f'{subj}/*.mp3')) + list(glob(f'{subj}/*.pth'))
-    return voices
-
-
 def load_voice(voice, extra_voice_dirs=[]):
     if voice == 'random':
         return None, None
     files = s3.list_objects(Bucket=bucket_name, Prefix=f"voices/{voice}/")["Contents"]
 
-    # voices = get_voices(extra_voice_dirs)
-    # paths = voices[voice]
-    # files
-    # paths = s3.get_object(Bucket=bucket_name, Key="voices")
-    # if len(paths) == 1 and paths[0].endswith('.pth'):
-    #     return None, torch.load(paths[0])
-    # else:
     conds = []
-    # for cond_path in paths:
-    #     c = load_audio(cond_path, 22050)
-    #     conds.append(c)
     for content in files:
         cond = np.frombuffer(s3.get_object(Bucket=bucket_name,
                                          Key=content["Key"])["Body"].read(),
